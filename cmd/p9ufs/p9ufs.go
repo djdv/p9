bcc32f1e9834e236e6fb65eeed8522b0d916a55a
Dominic Della Valle <ddvpublic@gmail.com>
2025-06-15 12:19:55 -0400

    p9ufs: improve command line help
    Utilize a `flag.FlagSet` and reduce parsing code at callsite (main).
    Normalize the executable name for printing in usage text.
    Use the output writer assigned to the flagSet rather than using fmt.
    Exit with `2` on flag parsing errors. This is a common convention.
    Revert changing output from stderr to stdout

--- cmd/p9ufs/p9ufs.go
+++ cmd/p9ufs/p9ufs.go
@@ -26,52 +26,32 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"net"
 	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/hugelgupf/p9/fsimpl/localfs"
 	"github.com/hugelgupf/p9/p9"
 	"github.com/u-root/uio/ulog"
 )
 
-var (
-	verbose = flag.Bool("v", false, "verbose logging")
-	root    = flag.String("root", "/", "root dir of file system to expose")
-	unix    = flag.Bool("unix", false, "use unix domain socket instead of TCP")
+const (
+	success int = iota
+	failure
+	misuse
 )
 
-// Prints custom help to document addr:port argument
-func Usage() {
-	fmt.Print("p9ufs - local 9P2000.L server in userspace\n\n")
-	fmt.Printf("usage: %s [options] <bind-addr:port>\n\noptions:\n", os.Args[0])
-	// print options to stdout
-	flag.CommandLine.SetOutput(os.Stdout)
-	flag.PrintDefaults()
-}
-
 func main() {
-	// 'flag' setup
-	// - disable flag usage to avoid printing it after errors
-	flag.Usage = func() {}
-	// - return errors to handle them manually
-	flag.CommandLine.Init("p9ufs", flag.ContinueOnError)
-	err := flag.CommandLine.Parse(os.Args[1:])
-	if err != nil {
-		// error is already printed to stderr at this point
-		if err == flag.ErrHelp {
-			// process -h, --help
-			Usage()
-			os.Exit(0)
-		}
-		os.Exit(1)
-	}
-	// - print usage if no params given
-	if len(flag.Args()) != 1 {
-		Usage()
-		os.Exit(0)
-	}
-
-	var network string
+	var (
+		flagSet = newFlagSet()
+		verbose = flagSet.Bool("v", false, "verbose logging")
+		root    = flagSet.String("root", "/", "root dir of file system to expose")
+		unix    = flagSet.Bool("unix", false, "use unix domain socket instead of TCP")
+		network string
+	)
+	parseFlags(flagSet, os.Args[1:])
 	if *unix {
 		network = "unix"
 	} else {
@@ -79,10 +59,10 @@ func main() {
 	}
 
 	// Bind and listen on the socket.
-	serverSocket, err := net.Listen(network, flag.Args()[0])
+	serverSocket, err := net.Listen(network, flagSet.Args()[0])
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "err binding: %v\n", err)
-		os.Exit(2)
+		os.Exit(failure)
 	}
 
 	var opts []p9.ServerOpt
@@ -92,4 +72,52 @@ func main() {
 	// Run the server.
 	s := p9.NewServer(localfs.Attacher(*root), opts...)
 	s.Serve(serverSocket)
+}
+
+func newFlagSet() *flag.FlagSet {
+	var (
+		execName = commandName(os.Args[0])
+		flagSet  = flag.NewFlagSet(execName, flag.ContinueOnError)
+	)
+	flagSet.Usage = newUsage(flagSet)
+	return flagSet
+}
+
+// commandName normalizes execPath, by
+// removing path components, file extensions, etc.
+func commandName(execPath string) string {
+	baseName := filepath.Base(execPath)
+	return strings.TrimSuffix(
+		baseName,
+		filepath.Ext(baseName),
+	)
+}
+
+func newUsage(flagSet *flag.FlagSet) func() {
+	return func() {
+		var (
+			output = flagSet.Output()
+			cmd    = commandName(os.Args[0])
+		)
+		io.WriteString(output, fmt.Sprintf(
+			"%s - local 9P2000.L server in userspace\n\n"+
+				"usage: %s [flags] <bind-addr:port>\n\n"+
+				"flags:\n",
+			cmd, cmd,
+		))
+		flagSet.PrintDefaults()
+	}
+}
+
+func parseFlags(flagSet *flag.FlagSet, arguments []string) {
+	if err := flagSet.Parse(arguments); err != nil {
+		if err == flag.ErrHelp {
+			os.Exit(success)
+		}
+		os.Exit(misuse)
+	}
+	if len(flagSet.Args()) != 1 {
+		flagSet.Usage()
+		os.Exit(misuse)
+	}
 }
